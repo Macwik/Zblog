9d69a990f2003ae6c854c723fb07937cc6115573
zhang <zhcszu@126.com>
2017-03-31 19:00:58 +0800

    基本可用

--- src/main/java/com/zblog/web/backend/form/LoginForm.java
+++ src/main/java/com/zblog/web/backend/form/LoginForm.java
@@ -1,51 +1,57 @@
 package com.zblog.web.backend.form;
 
-public class LoginForm{
-  private String username;
-  private String password;
-  private boolean remeber;
-  /* 防止后台暴力破解的字段 */
-  private String guard;
-  private String redirectURL;
+public class LoginForm {
+	private String username;
+	private String password;
+	private boolean remeber;
+	/* 防止后台暴力破解的字段 */
+	private String guard;
+	private String redirectURL;
 
-  public String getUsername(){
-    return username;
-  }
+	public String getUsername() {
+		return username;
+	}
 
-  public void setUsername(String username){
-    this.username = username;
-  }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-  public String getPassword(){
-    return password;
-  }
+	public String getPassword() {
+		return password;
+	}
 
-  public void setPassword(String password){
-    this.password = password;
-  }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-  public boolean isRemeber(){
-    return remeber;
-  }
+	public boolean isRemeber() {
+		return remeber;
+	}
 
-  public void setRemeber(boolean remeber){
-    this.remeber = remeber;
-  }
+	public void setRemeber(boolean remeber) {
+		this.remeber = remeber;
+	}
 
-  public String getGuard(){
-    return guard;
-  }
+	public String getGuard() {
+		return guard;
+	}
 
-  public void setGuard(String guard){
-    this.guard = guard;
-  }
+	public void setGuard(String guard) {
+		this.guard = guard;
+	}
 
-  public String getRedirectURL(){
-    return redirectURL;
-  }
+	public String getRedirectURL() {
+		return redirectURL;
+	}
 
-  public void setRedirectURL(String redirectURL){
-    this.redirectURL = redirectURL;
-  }
+	public void setRedirectURL(String redirectURL) {
+		this.redirectURL = redirectURL;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginForm [username=" + username + ", password=" + password + ", remeber=" + remeber + ", guard="
+				+ guard + ", redirectURL=" + redirectURL + "]";
+	}
 
 }
